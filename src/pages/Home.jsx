a1b2ff8848326bf9ff07f49e554597e6aed3610d
Christian Figueroa <christian-f79@hotmail.com>
2023-03-16 19:05:22 -0500

    Todos los archivos iniciales e indispensables de una aplicación para una tienda Online con React

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+    return (
+        <Header />
+        /*<div>
+            <h1>React Shop</h1>
+        </div> */
+    );
+}
+
+export default Home;
